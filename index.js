c1c4a89e4616d02e440928f71358e177dfe06f56
shad mustafa <shadmanmi2018@gmail.com>
2022-12-05 19:53:20 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(str = "roller-skate") {
+    return "This Saturday, I want to " + str + "!";
+}
+function mondayWork(str = "go to the office") { return "This Monday, I will " + str + "." }
+
+
+
+
+let wrapAdjective = function (strr = "*") {
+    // if (strr == "*") {
+    return (function inner(str) {
+        return "You are " + strr + str + strr + "!"
+    })
+    // }
+    // else if (strr == "||") {
+    //         return ("||" + function inner(str = "special") {
+    //             return "You are " + str + strr + "!"
+    //         })
+    //     }
+}
+const encouragingPromptFunction = wrapAdjective("!!!")
